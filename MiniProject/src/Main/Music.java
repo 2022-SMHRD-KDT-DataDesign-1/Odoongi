715ce80bf5207c0125e1b5734e6a0a14bf43b504
gonixxxx <akdntm946@gmail.com>
2023-01-27 10:04:16 +0900

    test

--- MiniProject/src/Main/Music.java
+++ MiniProject/src/Main/Music.java
@@ -5,7 +5,6 @@ import javazoom.jl.player.MP3Player;
 
 public class Music {
 
-<<<<<<< HEAD
 	private String musicPath;
 
 	public String getMusicPath() {
@@ -35,34 +34,4 @@ public class Music {
 	}
 
 }
-=======
-   private String musicPath;
 
-   public String getMusicPath() {
-      return musicPath;
-   }
-
-   public void setMusicPath(String musicPath) {
-      this.musicPath = musicPath;
-   }
-
-   MP3Player player = new MP3Player();
-
-   public Music(String musicPath) {
-        super();
-        this.musicPath = musicPath;
-   }
-
-   // 재생기능
-   public void play(String s) {
-      player.play(s);
-      
-   }
-
-   // 정지 기능
-   public void stop() {
-      player.stop();
-   }
-
-}
->>>>>>> 1480cc793c25309f5038ed96a2ede8603320304c
